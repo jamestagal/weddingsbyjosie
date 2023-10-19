30d64996860640832f6a240e0127e2d1266e6374
jamestagal <benjaminjameswaller@gmail.com>
2023-10-19 17:58:30 +1100

    create contact us page

---
+++ layouts/components/contact.svelte
@@ -0,0 +1,191 @@
+<!---contact-us-section-->
+<section class="ttm-row contact-us-section bg-layer-equal-height clearfix">
+  <div class="container">
+    <!--row-->
+    <div class="row">
+      <div class="col-lg-12">
+        <div class="row no-gutters">
+          <div class="col-lg-5 col-md-5 col-sm-12">
+            <div class="ttm-col-bgcolor-yes ttm-bg">
+              <div class="ttm-col-wrapper-bg-layer ttm-bg-layer" />
+              <div class="layer-content">
+                <!-- section title -->
+                <div class="section-title without-seperator">
+                  <div class="title-header">
+                    <h5>Wedding Services</h5>
+                    <h2 class="title">It’s Your Turn</h2>
+                  </div>
+                </div>
+                <!-- section title end -->
+                <p>
+                  There is no obligation when contacting me. You are most
+                  welcome to have a chat to find out whether my service is right
+                  for you.
+                </p>
+                <!-- featured-icon-box -->
+                <div
+                  class="featured-icon-box icon-align-before-content icon-ver_align-top style8"
+                >
+                  <div class="featured-icon">
+                    <div
+                      class="ttm-icon ttm-icon_element-onlytxt ttm-icon_element-color-skincolor ttm-icon_element-size-sm"
+                    >
+                      <i class="ti ti-location-pin" />
+                    </div>
+                  </div>
+                  <div class="featured-content">
+                    <div class="featured-title">
+                      <h5>Location</h5>
+                    </div>
+                    <div class="featured-desc">
+                      <p>Mission Street<br />Amaroo, ACT</p>
+                    </div>
+                  </div>
+                </div>
+                <!-- featured-icon-box end -->
+                <!-- featured-icon-box -->
+                <div
+                  class="featured-icon-box icon-align-before-content icon-ver_align-top style8"
+                >
+                  <div class="featured-icon">
+                    <div
+                      class="ttm-icon ttm-icon_element-onlytxt ttm-icon_element-color-skincolor ttm-icon_element-size-sm"
+                    >
+                      <i class="ti ti-files" />
+                    </div>
+                  </div>
+                  <div class="featured-content">
+                    <div class="featured-title">
+                      <h5>Call Or Email</h5>
+                    </div>
+                    <div class="featured-desc">
+                      <p>+61 406 379 530<br />weddingsbyjosiecbr@gmail.com</p>
+                    </div>
+                  </div>
+                </div>
+                <!-- featured-icon-box end -->
+                <!-- featured-icon-box -->
+                <div
+                  class="featured-icon-box icon-align-before-content icon-ver_align-top style8"
+                >
+                  <div class="featured-icon">
+                    <div
+                      class="ttm-icon ttm-icon_element-onlytxt ttm-icon_element-color-skincolor ttm-icon_element-size-sm"
+                    >
+                      <i class="ti ti-briefcase" />
+                    </div>
+                  </div>
+                  <div class="featured-content">
+                    <div class="featured-title">
+                      <h5>Office Hours</h5>
+                    </div>
+                    <div class="featured-desc">
+                      <p>Mon – Sun: 9.00am to 8.00pm</p>
+                    </div>
+                  </div>
+                </div>
+                <!-- featured-icon-box end -->
+              </div>
+            </div>
+          </div>
+          <div class="col-lg-7 col-md-7 col-sm-12">
+            <div class="border pt-35 pr-30 pb-40 pl-30">
+              <h5>Do You Have Any Questions?</h5>
+              <form
+                id="contactform"
+                class="contactform wrap-form pt-5 clearfix"
+                method="post"
+                novalidate="novalidate"
+                action="#"
+              >
+                <div class="row">
+                  <div class="col-md-6">
+                    <label>
+                      <span class="text-input"
+                        ><input
+                          name="name"
+                          type="text"
+                          value=""
+                          placeholder="Your Name*"
+                          required="required"
+                        /></span
+                      >
+                    </label>
+                  </div>
+                  <div class="col-md-6">
+                    <label>
+                      <span class="text-input"
+                        ><input
+                          name="address"
+                          type="text"
+                          value=""
+                          placeholder="Your Email*"
+                          required="required"
+                        /></span
+                      >
+                    </label>
+                  </div>
+                </div>
+                <div class="row">
+                  <div class="col-md-6">
+                    <label>
+                      <span class="text-input"
+                        ><input
+                          name="phone"
+                          type="text"
+                          value=""
+                          placeholder="Phone"
+                          required="required"
+                        /></span
+                      >
+                    </label>
+                  </div>
+                  <div class="col-md-6">
+                    <label>
+                      <span class="text-input"
+                        ><input
+                          name="phone"
+                          type="text"
+                          value=""
+                          placeholder="your-event"
+                          required="required"
+                        /></span
+                      >
+                    </label>
+                  </div>
+                </div>
+                <label>
+                  <span class="text-input"
+                    ><input
+                      name="phone"
+                      type="text"
+                      value=""
+                      placeholder="Subject"
+                      required="required"
+                    /></span
+                  >
+                </label>
+                <label>
+                  <span class="text-input"
+                    ><textarea
+                      name="message"
+                      rows="4"
+                      placeholder="Your Messages"
+                      required="required"
+                    /></span
+                  >
+                </label>
+                <button
+                  class="submit ttm-btn ttm-btn-size-md ttm-btn-shape-rounded ttm-btn-style-fill ttm-btn-color-skincolor w-100"
+                  type="submit">Send A Message!</button
+                >
+              </form>
+            </div>
+          </div>
+        </div>
+        <!-- row end -->
+      </div>
+    </div>
+  </div>
+</section>
+<!--contact-us-section end-->
