d8c41f14ce47b984135fc259943937c5ad0a5e89
jamestagal <benjaminjameswaller@gmail.com>
2023-11-02 21:58:35 +1100

    create alternate price comp

---
+++ layouts/components/alternate_price.svelte
@@ -0,0 +1,58 @@
+<!--welcome-section-->
+<section
+  class="ttm-row welcome-section bg-layer-equal-height break-991-colum clearfix"
+>
+  <div class="container">
+    <!-- row -->
+    <div class="row">
+      <div class="col-lg-2 col-md-12">
+        <!--col-bg-img-one-->
+        <div
+          class="col-bg-img-one ttm-bg ttm-col-bgimage-yes ttm-bgcolor-grey spacing-2"
+        >
+          <div class="ttm-col-wrapper-bg-layer ttm-bg-layer">
+            <div class="ttm-bg-layer-inner" />
+          </div>
+          <div class="layer-content">
+            <!--section title-->
+            <div class="section-title">
+              <div class="title-header">
+                <h5>Alternate Wedding Service - $ negotiable</h5>
+                <h2 class="title">
+                    The Intimate Wedding
+                </h2>
+              </div>
+              <div class="title-desc">
+                <p>
+                    Is a smaller, intimate wedding. You will get a beautiful, personalised ceremony like a registry style service. This is perfect for those who want to legalise their union without a traditional wedding but do it in a more affordable way. I still provide a meaningful and memorable ceremony.
+                </p>
+                <p>
+                    After the ceremony, I will lodge all the paperwork with the Births, Deaths and Marriages Department, in the state or territory where the wedding was conducted. Then I Inform you when the marriage has been registered and send you the link so that you can apply for the official document.
+                </p>
+              </div>
+            </div>
+            <!--section title end-->
+          </div>
+        </div>
+        <!--col-bg-img-one end-->
+      </div>
+      <div class="col-lg-10 col-md-12">
+        <!--col-img-img-two-->
+        <div
+          class="col-bg-img-two ttm-col-bgimage-yes ttm-bg spacing-1 z-index-0"
+        >
+          <div class="ttm-col-wrapper-bg-layer ttm-bg-layer" />
+          <div class="layer-content" />
+        </div>
+        <!--col-img-bg-img-two end-->
+        <img
+          src="media/IntimateWedding.jpeg"
+          class="ttm-equal-height-image"
+          alt="Josie at an intimate wedding"
+        />
+      </div>
+    </div>
+    <!-- row end -->
+  </div>
+</section>
+<!--welcome-section end-->
