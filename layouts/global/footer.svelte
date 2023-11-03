0b98822a3b550054967e4ef096f9db4ecec8962a
jamestagal <benjaminjameswaller@gmail.com>
2023-11-03 18:59:54 +1100

    update footer

--- layouts/global/footer.svelte
+++ layouts/global/footer.svelte
@@ -18,7 +18,7 @@
                 <div class="featured-box text-center">
                   <div class="featured-content">
                     <div class="featured-title">
-                      <h5>Our Address</h5>
+                      <h5>My Address</h5>
                     </div>
                     <div class="featured-desc">
                       <p>Gungahlin area, ACT</p>
@@ -30,7 +30,7 @@
                 <div class="featured-box text-center">
                   <div class="featured-content">
                     <div class="featured-title">
-                      <h5>Talk To Josie</h5>
+                      <h5>Talk To Me</h5>
                     </div>
                     <div class="featured-desc">
                       <p>0406 379 530</p>
@@ -42,7 +42,7 @@
                 <div class="featured-box text-center">
                   <div class="featured-content">
                     <div class="featured-title">
-                      <h5>Email Us</h5>
+                      <h5>Email Me</h5>
                     </div>
                     <div class="featured-desc">
                       <p>weddingsbyjosiecbr@gmail.com</p>
@@ -50,43 +50,6 @@
                   </div>
                 </div>
               </div>
-            </div>
-            <div class="row no-gutters">
-              <div class="col-md-3 col-sm-2" />
-              <div class="col-md-6 col-sm-8">
-                <form
-                  id="subscribe-form"
-                  class="newsletter-form"
-                  method="post"
-                  action="#"
-                  data-mailchimp="true"
-                >
-                  <div
-                    class="mailchimp-inputbox clearfix"
-                    id="subscribe-content"
-                  >
-                    <p>
-                      <i class="fa fa-envelope-o" />
-                      <input
-                        type="email"
-                        name="email"
-                        autocomplete="off"
-                        placeholder="Your Email Address.."
-                        required=""
-                      />
-                    </p>
-                    <p>
-                      <button
-                        class="submit ttm-btn ttm-btn-size-sm ttm-btn-shape-rounded ttm-btn-style-fill ttm-btn-color-skincolor ttm-icon-btn-right"
-                        type="submit"
-                        >Submit<i class="ti ti-arrow-right" /></button
-                      >
-                    </p>
-                  </div>
-                  <div id="subscribe-msg" />
-                </form>
-              </div>
-              <div class="col-md-3 col-sm-2" />
             </div>
           </div>
         </div>
@@ -96,16 +59,19 @@
   <div class="second-footer">
     <div class="container">
       <div class="row">
-        <div class="col-xs-12 col-sm-6 col-md-6 col-lg-4 widget-area">
+        <div class="col-xs-12 col-sm-6 col-md-6 col-lg-5 widget-area">
           <div class="widget widget_text clearfix">
-            <h3 class="widget-title">About Weddings by Josie</h3>
+            <h3 class="widget-title">Weddings by Josie</h3>
             <div class="textwidget widget-text">
-              I'm Josie, the heart behind Weddings by Josie, a wedding celebrant
-              based in Canberra, ACT. I provide high quality, meaningful wedding
-              services for all Canberrians. Whether you're planning a same-sex
-              marriage, a Naming Day ceremony, or a Renewal of Vows, I'm here to
-              create that special ceremony that just right for you.<br /><br />
+              A registered Canberra based marriage celebrant. I perform Wedding
+              ceremonies including same-sex marriages or a Naming Day or a
+              Renewal of Vows ceremony.
             </div>
+          </div>
+        </div>
+        <div class="col-xs-12 col-sm-6 col-md-6 col-lg-2 widget-area">
+          <div class="widget widget_nav_menu clearfix">
+            <h3 class="widget-title">Socials</h3>
             <div class="social-icons circle social-hover">
               <ul class="list-inline">
                 <li class="social-facebook">
@@ -152,155 +118,15 @@
             </div>
           </div>
         </div>
-        <div class="col-xs-12 col-sm-6 col-md-6 col-lg-2 widget-area">
+        <div class="col-xs-12 col-sm-6 col-md-6 col-lg-5 widget-area">
           <div class="widget widget_nav_menu clearfix">
-            <h3 class="widget-title">Our Services</h3>
-            <ul class="menu-footer-services">
-              <li><a href=".">User Strategy</a></li>
-              <li><a href=".">Product Designing</a></li>
-              <li><a href=".">Marketing Strategy</a></li>
-              <li><a href=".">IT Consultancy</a></li>
-              <li><a href=".">Server Security</a></li>
+            <h3 class="widget-title">Links</h3>
+            <ul class="my-inline-list menu-footer-services">
+              <li><a href="about-me">About me</a></li>
+              <li><a href="service">Services</a></li>
+              <li><a href="pricing">Pricing</a></li>
+              <li><a href="contact-me">Contact me</a></li>
             </ul>
-          </div>
-        </div>
-        <div class="col-xs-12 col-sm-6 col-md-6 col-lg-3 widget-area">
-          <div class="widget widget_nav_menu clearfix">
-            <h3 class="widget-title">Wedding Planner Faqs</h3>
-            <ul class="menu-footer-services">
-              <li><a href=".">Why hire a planner ?</a></li>
-              <li><a href=".">How can i make an appointment ?</a></li>
-              <li><a href=".">How to choose a venue ?</a></li>
-              <li><a href=".">Event catalogue ?</a></li>
-              <li><a href=".">Pyment type you accept</a></li>
-            </ul>
-          </div>
-        </div>
-        <div class="col-xs-12 col-sm-6 col-md-6 col-lg-3 widget-area">
-          <div class="widget flicker_widget clearfix">
-            <h3 class="widget-title">Instagram Gallery</h3>
-            <div class="widget_media_gallery">
-              <figure class="gallery-item">
-                <div class="ttm-box-view-overlay">
-                  <img
-                    class="img-fluid"
-                    src="https://via.placeholder.com/150x150?text=150x150+gallery-01.jpg"
-                    alt="Gallery"
-                    title="gallery-img"
-                  />
-                  <div class="ttm-media-link">
-                    <a
-                      class="ttm_prettyphoto ttm_image"
-                      title="gallery"
-                      data-rel="prettyPhoto"
-                      href="https://via.placeholder.com/770x770?text=770x770+pf-01.jpg"
-                    >
-                      <i class="ti ti-search" />
-                    </a>
-                  </div>
-                </div>
-              </figure>
-              <figure class="gallery-item">
-                <div class="ttm-box-view-overlay">
-                  <img
-                    class="img-fluid"
-                    src="https://via.placeholder.com/150x150?text=150x150+gallery-02.jpg"
-                    alt="Gallery"
-                    title="gallery-img"
-                  />
-                  <div class="ttm-media-link">
-                    <a
-                      class="ttm_prettyphoto ttm_image"
-                      title="gallery"
-                      data-rel="prettyPhoto"
-                      href="https://via.placeholder.com/770x770?text=770x770+pf-05.jpg"
-                    >
-                      <i class="ti ti-search" />
-                    </a>
-                  </div>
-                </div>
-              </figure>
-              <figure class="gallery-item">
-                <div class="ttm-box-view-overlay">
-                  <img
-                    class="img-fluid"
-                    src="https://via.placeholder.com/150x150?text=150x150+gallery-03.jpg"
-                    alt="Gallery"
-                    title="gallery-img"
-                  />
-                  <div class="ttm-media-link">
-                    <a
-                      class="ttm_prettyphoto ttm_image"
-                      title="gallery"
-                      data-rel="prettyPhoto"
-                      href="https://via.placeholder.com/770x770?text=770x770+pf-02.jpg"
-                    >
-                      <i class="ti ti-search" />
-                    </a>
-                  </div>
-                </div>
-              </figure>
-              <figure class="gallery-item">
-                <div class="ttm-box-view-overlay">
-                  <img
-                    class="img-fluid"
-                    src="https://via.placeholder.com/150x150?text=150x150+gallery-04.jpg"
-                    alt="Gallery"
-                    title="gallery-img"
-                  />
-                  <div class="ttm-media-link">
-                    <a
-                      class="ttm_prettyphoto ttm_image"
-                      title="gallery"
-                      data-rel="prettyPhoto"
-                      href="https://via.placeholder.com/770x770?text=770x770+pf-10.jpg"
-                    >
-                      <i class="ti ti-search" />
-                    </a>
-                  </div>
-                </div>
-              </figure>
-              <figure class="gallery-item">
-                <div class="ttm-box-view-overlay">
-                  <img
-                    class="img-fluid"
-                    src="https://via.placeholder.com/150x150?text=150x150+gallery-05.jpg"
-                    alt="Gallery"
-                    title="gallery-img"
-                  />
-                  <div class="ttm-media-link">
-                    <a
-                      class="ttm_prettyphoto ttm_image"
-                      title="gallery"
-                      data-rel="prettyPhoto"
-                      href="https://via.placeholder.com/770x770?text=770x770+pf-03-540x640.jpg"
-                    >
-                      <i class="ti ti-search" />
-                    </a>
-                  </div>
-                </div>
-              </figure>
-              <figure class="gallery-item">
-                <div class="ttm-box-view-overlay">
-                  <img
-                    class="img-fluid"
-                    src="https://via.placeholder.com/150x150?text=150x150+gallery-06.jpg"
-                    alt="Gallery"
-                    title="gallery-img"
-                  />
-                  <div class="ttm-media-link">
-                    <a
-                      class="ttm_prettyphoto ttm_image"
-                      title="gallery"
-                      data-rel="prettyPhoto"
-                      href="https://via.placeholder.com/770x770?text=770x770+pf-04-770x770.jpg"
-                    >
-                      <i class="ti ti-search" />
-                    </a>
-                  </div>
-                </div>
-              </figure>
-            </div>
           </div>
         </div>
       </div>
@@ -323,4 +149,26 @@
     </div>
   </div>
 </footer>
+
 <!--footer end-->
+<style>
+  .my-inline-list {
+    display: flex;
+    justify-content: space-between;
+  }
+  .footer .widget {
+    margin: 10px 0 10px;
+  }
+  @media (max-width: 991px) {
+    .second-footer .widget-area .widget {
+      padding: 0 0 10px;
+      margin: 0;
+    }
+  }
+  @media (max-width: 991px) {
+    .my-inline-list {
+      display: block ;
+      margin: 0;
+    }
+  }
+</style>
