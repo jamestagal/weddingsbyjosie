c65f9e78cd5550799d479a3a4aedac25116161b8
jamestagal <benjaminjameswaller@gmail.com>
2023-11-03 18:56:44 +1100

    add new comp about client

---
+++ layouts/components/josie.svelte
@@ -0,0 +1,27 @@
+<!--team detail-section_1-->
+<section class="ttm-row team-detail-section_1 clearfix">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="ttm-team-member-single-content-wrapper">
+                    <div class="row">
+                        <div class="col-md-5">
+                            <!--featured-thumbnail-->
+                            <div class="featured-thumbnail pr-25 res-991-pr-0 ">
+                                <img class="img-fluid res-767-mb-40" src="media/JosieAlloccaFeature.jpg" alt="Josie Allocca">
+                            </div><!--featured-thumbnail end-->
+                        </div>
+                        <div class="col-md-7 d-flex align-items-center">
+                            <div class="ttm-team-member-single-list">
+                                <h2 class="ttm-team-member-single-title">Josie Allocca</h2>
+                                <h5 class="ttm-team-member-single-position ttm-textcolor-skincolor">ACT based Wedding Celebrant</h5>
+                                <p>I'm Josie, the heart behind Weddings by Josie, a wedding celebrant based in Canberra, ACT and I am happy to travel to regional NSW. I provide personalised ceremony that reflects your vision. Get in touch and let's make your wedding a special and memorable day.</p>
+                            </div>
+                        </div>
+                    </div>   
+                </div>
+            </div>
+        </div>                 
+    </div>
+</section>
+<!--team detail-section_1 end-->
