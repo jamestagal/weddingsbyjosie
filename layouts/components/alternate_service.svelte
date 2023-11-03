0cb37ef865bb9b8df8bcbbea4605c750ad53facc
jamestagal <benjaminjameswaller@gmail.com>
2023-11-03 18:53:46 +1100

    update alternate service page

--- layouts/components/alternate_service.svelte
+++ layouts/components/alternate_service.svelte
@@ -1,74 +1,74 @@
-            <!--about-section_1-->
-            <section class="ttm-row about-section_1 clearfix">
-                <div class="container">
-                    <!--row-->
-                    <div class="row">
-                        <div class="col-lg-6 col-sm-12">
-                            <!--ttm_single_image-wrapper-->
-                            <div class="ttm_single_image-wrapper">
-                                <img class="img-fluid" src="media/service/single-img-02.png" title="Collage of two wedding images" alt="single-img-two">
-                            </div><!--ttm_single_image-wrapper end-->
-                        </div>
-                        <div class="col-lg-6 col-sm-12">
-                            <div class="pt-35 pl-25 res-991-pl-0 res-991-pt-30">
-                                <!--section title-->
-                                <div class="section-title">
-                                    <div class="title-header">
-                                        <h5>Alternate Wedding Service - $ negotiable</h5>
-                                        <h2 class="title">The Intimate Wedding</h2>
-                                    </div>
-                                    <div class="title-desc">
-                                        <p>A cheaper option, if you want a simple, affordable but still personal ceremony. You may be
-                                            having your religious ceremony here or overseas with family and friends, or simply want
-                                            to get married with no frills. I prepare all the legal paperwork, this paperwork must be completed a minimum of 1 month
-                                            before the ceremony. This is a legal reqirement.</p>
-                                    </div>
-                                </div><!--section title end-->
-                                <p>I’ll write a personal ceremony for you and perform the face to face ceremony which is also
-                                    part of the legal process. This ceremony may be at a location of your choice or my home,
-                                    with a small number of guests.</p>
-                                <div class="pt-5">
-
-                                    <!--featured-icon-box-->
-                                    <div class="featured-icon-box icon-align-before-content icon-ver_align-top style4">
-                                        <div class="featured-icon">
-                                            <div class="ttm-icon ttm-icon_element-onlytxt ttm-icon_element-color-skincolor ttm-icon_element-size-lg">
-                                                <i class="flaticon flaticon-balloons"></i>
-                                            </div>
-                                        </div>
-                                        <div class="featured-content">
-                                            <div class="featured-title">
-                                                <h5>The Ceremony</h5>
-                                            </div>
-                                            <div class="featured-desc">
-                                                <p>You will get a beautiful, personalised and intimate ceremony like a registry style service. This is perfect for those who want to legalise their union without a traditional wedding but do it in an affordable way yet still in a meaningful ceremony.</p>
-                                            </div>
-                                        </div>
-                                    </div><!--featured-icon-box end-->
-
-                                     <div class="ttm-horizontal_sep mt-25 mb-30"></div>
-
-                                    <!--featured-icon-box-->
-                                    <div class="featured-icon-box icon-align-before-content icon-ver_align-top style4">
-                                        <div class="featured-icon">
-                                            <div class="ttm-icon ttm-icon_element-onlytxt ttm-icon_element-color-skincolor ttm-icon_element-size-lg">
-                                                <i class="flaticon flaticon-love-birds"></i>
-                                            </div>
-                                        </div>
-                                        <div class="featured-content">
-                                            <div class="featured-title">
-                                                <h5>The Legals</h5>
-                                            </div>
-                                            <div class="featured-desc">
-                                                <p>I will lodge all the paperwork with the Births, Deaths and Marriages Department, in the state or territory where the wedding was conducted. Then I Inform you when the marriage has been registered and send you the link so that
-                                                    you can apply for the official document. </p>
-                                            </div>
-                                        </div>
-                                    </div><!--featured-icon-box end-->
+ <!--about-section-->
+ <section class="ttm-row about-section bg-img6 ttm-bg ttm-bgimage-yes clearfix">
+    <div class="container">
+        <!--row end-->
+        <div class="row">
+            <div class="col-lg-6">
+                <div class="d-inline-block border p-3">
+                   <!--ttm_single_image-wrapper-->
+                    <div class="ttm_single_image-wrapper">
+                        <img class="img-fluid" src="media/service/WeddingCeremonyAlt.webp" title="single-img-three" alt="single-img-three">
+                    </div><!--ttm_single_image-wrapper end-->
+                </div>
+            </div>
+            <div class="col-lg-6">
+                <div class="res-991-pt-40 pr-15">
+                <!--section title-->
+                <div class="section-title">
+                    <div class="title-header">
+                        <h5>Alternate Wedding Service - $ negotiable</h5>
+                        <h2 class="title">The Intimate Wedding</h2>
+                    </div>
+                    <div class="title-desc">
+                        <p>A cheaper option, if you want a simple, affordable but still personal ceremony. You may be
+                            having your religious ceremony here or overseas with family and friends, or simply want
+                            to get married with no frills. </p>
+                            <p>I’ll provide a personal ceremony for you and perform the face to face ceremony which is also compulsory as part of the legal process. This ceremony may be at a location of your choice or my home, with a small number of guests.</p>
+                    </div>
+                </div><!--section title end-->
+                <div class="row">
+                    <div class="col-md-6">
+                        <!--featured-icon-box-->
+                        <div class="featured-icon-box icon-align-before-title style6">
+                            <div class="featured-icon">
+                                <div class="ttm-icon ttm-icon_element-fill ttm-icon_element-color-skincolor ttm-icon_element-size-sm ttm-icon_element-style-rounded">
+                                    <i class="flaticon flaticon-ring"></i>
                                 </div>
                             </div>
-                        </div>
-                    </div><!--row end-->
+                            <div class="featured-title">
+                                <h5>The Ceremony</h5>
+                            </div>
+                            <div class="featured-content">
+                                <div class="featured-desc">
+                                    <p>You will get a beautiful, personalised and intimate ceremony like a registry style service. This is perfect for those who want to legalise their union without a traditional wedding but do it in an affordable way yet still in a meaningful ceremony.</p>
+                                </div>
+                            </div>
+                        </div><!--featured-icon-box end-->
+                    </div>
+                    <div class="col-md-6">
+                        <!--featured-icon-box-->
+                        <div class="featured-icon-box icon-align-before-title style6">
+                            <div class="featured-icon">
+                                <div class="ttm-icon ttm-icon_element-fill ttm-icon_element-color-skincolor ttm-icon_element-size-sm ttm-icon_element-style-rounded">
+                                    <i class="flaticon flaticon-wedding-cake"></i>
+                                </div>
+                            </div>
+                            <div class="featured-title">
+                                <h5>The Legals</h5>
+                            </div>
+                            <div class="featured-content">
+                                <div class="featured-desc">
+                                    <p>BEFORE: I prepare all the legal paperwork, this paperwork must be completed a minimum of 1 month before the ceremony. This is a legal reqirement.
+                                        <br>AFTER: I will lodge all the paperwork with the ACT Births, Deaths and Marriages Department. Then I Inform you when the marriage has been registered and send you the link so that
+                                        you can apply for the official document. </p>
+                                </div>
+                            </div>
+                        </div><!--featured-icon-box end-->
+                    </div>
                 </div>
-            </section>
-            <!--about-section_1 end-->
+                </div>
+            </div>
+        </div><!-- row end -->
+    </div>
+</section>
+<!--about-section end-->
