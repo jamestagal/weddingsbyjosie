7e0636cd24d0dac2cd7f721407473f6f7c1a9904
jamestagal <benjaminjameswaller@gmail.com>
2023-11-09 10:15:09 +1100

    format description

--- layouts/components/josie.svelte
+++ layouts/components/josie.svelte
@@ -15,7 +15,7 @@
                             <div class="ttm-team-member-single-list">
                                 <h2 class="ttm-team-member-single-title">Josie Allocca</h2>
                                 <h5 class="ttm-team-member-single-position ttm-textcolor-skincolor">ACT based Wedding Celebrant</h5>
-                                <p>I'm Josie, the heart behind Weddings by Josie, a wedding celebrant based in Canberra, ACT and I am happy to travel to regional NSW. I provide personalised ceremony that reflects your vision. Get in touch and let's make your wedding a special and memorable day.</p>
+                                <p>I'm Josie, the heart behind Weddings by Josie, a wedding celebrant based in Canberra, ACT and I am happy to travel to regional NSW. I provide personalised ceremonies that reflect your vision. Get in touch and let's make your wedding a special and memorable day.</p>
                             </div>
                         </div>
                     </div>   
