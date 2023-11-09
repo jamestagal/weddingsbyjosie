165d87e7a1cb0c9cf9105ec5b682c3c4f19abc40
jamestagal <benjaminjameswaller@gmail.com>
2023-11-09 15:21:05 +1100

    add gitlab login auth

--- layouts/global/html.svelte
+++ layouts/global/html.svelte
@@ -1,14 +1,25 @@
 <script>
+  import { onMount } from 'svelte';
   import Head from "./head.svelte";
   import Header from "./header.svelte";
   import Footer from "./footer.svelte";
   import { makeTitle } from "../scripts/make_title.svelte";
-  export let content, layout, env, allContent, allLayouts;
+  import Login from "./login.svelte";
+  export let content, layout, env, allContent, allLayouts, user;
+
+  let hash;
+  onMount(async () => {
+    hash = window.location.hash;
+  });
 </script>
 
 <html lang="en">
   <Head title={makeTitle(content.filename)} {env} />
   <body>
+    {#if user && $user.isAuthenticated}
+      <svelte:component this={$user.menu} {user} bind:content />
+    {/if}
+    <Login bind:hash {user} />
     <main class="page">
       <Header />
       <div class="site-main">
